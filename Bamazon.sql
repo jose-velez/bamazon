04d3bc43d9b0d3f10a296a9bf785cc4e4995d05e
Jose Velez <empire@Joses-MBP-2.lan>
2017-04-17 11:15:38 -0400

    Database complete

--- Bamazon.sql
+++ Bamazon.sql
@@ -10,3 +10,33 @@ CREATE TABLE products (
   stock_quantity INT(11) NOT NULL,
   PRIMARY KEY (item_id)
 );
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Echo Dot", "Electronic", 49.99, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Contigo AutoSeal Travel Mug", "Kitchen", 10.78, 25);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Surge Protector", "Electronic", 21.59, 35);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Anker Wireless Charger", "Electronic", 9.99, 89);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Laptop Sleve", "Electronic",10.99, 12);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Garmin Nuvi GPS", "Electronic", 79.99, 5);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Costzon 8-Piece Nonstick Cookware Set", "Kitchen", 29.99, 100);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Crock-Pot 7-Quart Oval Manual Slow Cooker", "Kitchen", 17.00, 50);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Fujifilm Instax Mini 9 Instant Camera", "Electronic", 69.95, 33);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("Dolica AX620B100 62-Inch Proline Tripod and Ball Head", "Electronic", 49.99, 35);
